6a24c9ff0e8af12a72d4903424869b2f0a1eace7
Sabrina Frost <sabrina.frost@1password.com>
2020-01-01 11:07:45 -0800

    FIxed assets in build folder

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,7 @@ const port = Number(process.env.PORT) || 8000;
 const app: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify(serverOptions);
 const openApiOptions = {
   specification: `${__dirname}/assets/common/reference/frost.tools.v1.yaml`,
-  service: `${__dirname}/service.ts`,
+  service: `${__dirname}/service.${process.env.PORT === 'development' ? `ts` : `js`}`,
   prefix: "v1"
 };
 
